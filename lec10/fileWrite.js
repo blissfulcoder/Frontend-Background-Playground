8aee67a493a27d4244e3de08bbdf264ab71d768e
Deepesh <deepeshdg28@gmail.com>
2018-04-29 19:08:44 +0530

    updated code

---
+++ lec10/fileWrite.js
@@ -0,0 +1,11 @@
+const fs = require("fs")
+
+fs.writeFile(__dirname+"/write.txt","This is some text",
+function () {
+    console.info("Task finished")
+})
+
+fs.readFile(__dirname+"/write.txt",(err,data)=>{
+    if(err) throw err;
+    console.log(data.toString());
+})
