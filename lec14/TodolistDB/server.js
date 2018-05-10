a6572273d7bfb7fc04d084d774a96845e69dd748
Deepesh <deepeshdg28@gmail.com>
2018-05-10 20:23:16 +0530

    updated code

--- lec14/TodolistDB/server.js
+++ lec14/TodolistDB/server.js
@@ -3,15 +3,14 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express()
 
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-
 app.use('/',express.static(path.join(__dirname,'frontend')));
 
-app.post('todos/add',(req,res)=>{
-    console.log(req)
-    console.log(req.body.newtodo)
-}
-)
+app.post('/todos/add',(req,res)=> {
+    console.log(req);
+    console.log(req.body.newodo)
+})
 
-app.listen(5454, () => console.log("server started"));
+app.listen(5454, () => console.log("server started")
+)
