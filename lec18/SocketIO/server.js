4883541120d082bb82605baa87b5fb12eba80c04
Deepesh <deepeshdg28@gmail.com>
2018-05-18 08:22:08 +0530

    updated

---
+++ lec18/SocketIO/server.js
@@ -0,0 +1,19 @@
+const socketio = require('socket.io')
+const http = require('http')
+
+const app = express()
+
+const server = http.Server(app)
+
+const io = socketio(server)
+
+io.on('connection',function (socket) {
+
+})
+
+app.use('/',express.static(__dirname+ '/public_static'))
+
+server.listen(2323, () => {
+    console.log("Server started at htttp:///localhost:2323")
+
+})
