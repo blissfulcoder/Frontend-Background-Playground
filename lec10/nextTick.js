2dc882c836c13819b343be87f4a15c565cf07d17
Deepesh <deepeshdg28@gmail.com>
2018-07-06 23:36:45 +0530

    updated

---
+++ lec10/nextTick.js
@@ -0,0 +1,10 @@
+process.nextTick(() => {
+    console.info('next tick')
+process.nextTick(() => {
+    console.info('next of next tick')
+})
+})
+
+process.nextTick(() => {
+    console.info('this tick')
+})
