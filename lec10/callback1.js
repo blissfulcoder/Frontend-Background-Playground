f260cbdc7e4e43f363f7a527c338552cab2857d0
Deepesh <deepeshdg28@gmail.com>
2018-07-11 23:54:30 +0530

    updated

---
+++ lec10/callback1.js
@@ -0,0 +1,14 @@
+function one(a, cb1){
+    console.log(a)
+    cb1(a+1,function (c,cb3) {
+        console.log(c)
+        cb3(c+1)
+    })
+}
+
+one(1,function (b,cb2) {
+    console.log(b)
+    cb2(b+1,function (d) {
+        console.log(d)
+    })
+})
