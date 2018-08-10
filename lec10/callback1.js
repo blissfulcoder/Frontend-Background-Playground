ea1840bfab0c3c38b4db10fbef8f1d8d6fd74dc4
Deepesh <deepeshdg28@gmail.com>
2018-08-10 21:44:43 +0530

    updated

--- lec10/callback1.js
+++ lec10/callback1.js
@@ -4,11 +4,11 @@ function one(a, cb1){
         console.log(c)
         cb3(c+1)
     })
-}
+};
 
 one(1,function (b,cb2) {
     console.log(b)
     cb2(b+1,function (d) {
         console.log(d)
     })
-})
+});
